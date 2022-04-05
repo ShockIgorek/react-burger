a6683d558eb905724d8850b584e0494bf16416d5
ShockIgorek <shokin1996@yandex.ru>
2022-04-06 00:08:13 +0700

    feat icon

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,34 +1,22 @@
 import React from 'react';
 import styles from './app-header.module.css';
-console.log(styles)
+import {Logo,
+    BurgerIcon,
+    ListIcon,
+    ProfileIcon
+} from '@ya.praktikum/react-developer-burger-ui-components'
+const temporaryLink = '#'
 
-
-
-
+console.log(styles);
+console.log(ListIcon)
 export default AppHeader;
 function AppHeader() {
     return <header className={styles.appHeader}>
-        <ul className={styles.links}>
-            <li>
-                <a>
-                    Конструктор
-                </a>
-            </li>
-            <li>
-                <a>
-                    Лента заказов
-                </a>
-            </li>
-            <li>
-                <a>
-                    logo
-                </a>
-            </li>
-            <li>
-                <a>
-                    Личный кабинет
-                </a>
-            </li>
-        </ul>
-    </header>
+        <nav className={styles.appHeader__links}>
+            <a className={`pl-4 pr-4 pb-4 pt-4`}href={temporaryLink}><BurgerIcon className={'mr-2'}/>Конструктор</a>
+            <a className={`pl-4`}href={temporaryLink}><ListIcon className={'mr-2'}/>Лента заказов</a>
+            <a className={styles.appHeader__logo} href={temporaryLink}><Logo /></a>
+            <a href={temporaryLink}><ProfileIcon className={'mr-2'} />Личный кабинет</a>
+        </nav>
+    </header >
 }
