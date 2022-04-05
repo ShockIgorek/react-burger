65043a3b1b5d1769b624898c453a107e49d13ac4
ShockIgorek <shokin1996@yandex.ru>
2022-04-06 01:19:34 +0700

    feat grid

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import styles from './app-header.module.css';
-import {Logo,
+import {
+    Logo,
     BurgerIcon,
     ListIcon,
     ProfileIcon
 } from '@ya.praktikum/react-developer-burger-ui-components'
 const temporaryLink = '#'
 
-console.log(styles);
-console.log(ListIcon)
 export default AppHeader;
 function AppHeader() {
     return <header className={styles.appHeader}>
         <nav className={styles.appHeader__links}>
-            <a className={`pl-4 pr-4 pb-4 pt-4`}href={temporaryLink}><BurgerIcon className={'mr-2'}/>Конструктор</a>
-            <a className={`pl-4`}href={temporaryLink}><ListIcon className={'mr-2'}/>Лента заказов</a>
-            <a className={styles.appHeader__logo} href={temporaryLink}><Logo /></a>
-            <a href={temporaryLink}><ProfileIcon className={'mr-2'} />Личный кабинет</a>
+            <a className={`text text_type_main-default ${styles.appHeader__link}`} href={temporaryLink}>
+                <BurgerIcon />
+                Конструктор
+            </a>
+            <a className={`text text_type_main-default ${styles.appHeader__link}`} href={temporaryLink}>
+                <ListIcon />
+                Лента заказов
+            </a>
+            <a className={styles.appHeader__logo} href={temporaryLink}>
+                <Logo />
+            </a>
+            <a className={`text text_type_main-default ${styles.appHeader__link} ${styles.appHeader__profile}`} href={temporaryLink}>
+                <ProfileIcon />
+                Личный кабинет
+            </a>
         </nav>
     </header >
 }
