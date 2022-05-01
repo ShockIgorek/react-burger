0f14fbe2a923f84743e8072debfae8769514cedd
ShockIgorek <shokin1996@yandex.ru>
2022-05-01 22:21:32 +0700

    feat POST func

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -8,24 +8,12 @@ import { Ingredients } from '../services/ingredients'
 export default function BurgerIngredients({ setIngredientPopup, setSelectedIngredient, targetIngredients, setTargetIngredients }) {
     const startIngredients = useContext(Ingredients);
     console.log(startIngredients)
-    const [current, setCurrent] = useState('bun')
-
-    // const onClick = (evt) => {
-    //     const id = evt.currentTarget.dataset.id
-    //     const foundIngredient = ingredients.find(ingredient => ingredient._id === id)
-    //     setSelectedIngredient(foundIngredient)
-    //     setIngredientPopup(true)
-    // }
-    const detectCurrentTipeTitle = (type) => {
-        if (type === 'sauce') {
-            return 'Соусы'
-        } else if (type === 'main') {
-            return 'Начинки'
-        }
-        return 'Булки'
-    }
-
-    const handleIngredientDoubleClick = (event) => {
+    // реализовано добавление ингредиента при клике
+    const onClick = (event) => {
+        const id = event.currentTarget.dataset.id
+        const foundIngredient = startIngredients.find(ingredient => ingredient._id === id)
+        setSelectedIngredient(foundIngredient);
+        setIngredientPopup(true);
         const targetIngredient = startIngredients.find(ingredient => ingredient._id === event.currentTarget.dataset.id)
         const selectedBun = targetIngredients.find(ingredient => ingredient.type === 'bun')
         const selectedBunIndex = targetIngredients.indexOf(selectedBun)
@@ -37,13 +25,13 @@ export default function BurgerIngredients({ setIngredientPopup, setSelectedIngre
         } else {
             setTargetIngredients([...targetIngredients, targetIngredient])
         }
-
     }
+
 
     const itemTemplate = ({ image, price, name, _id }) => {
         let ingredientCounter = 0;
         targetIngredients.forEach(ingredient => ingredient.name === name && (ingredient.type === 'bun' ? ingredientCounter += 2 : ingredientCounter += 1))
-        return (<li data-id={_id} key={_id} onClick={handleIngredientDoubleClick} className={style.ingredient}>
+        return (<li data-id={_id} key={_id} onClick={onClick} className={style.ingredient}>
             <img alt={name} src={image} className={`${style.image} ml-4 mr-4`} />
             <div className={`${style.price_info} mt-4 mb-4`}>
                 <span className="text text_type_digits-default mr-2">
@@ -62,20 +50,28 @@ export default function BurgerIngredients({ setIngredientPopup, setSelectedIngre
         <div className={style.constructor}>
             <h1 className="mt-10 mb-5 text text_type_main-large">Соберите бургер</h1>
             <div className={style.header}>
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                <Tab value="bun">
                     Булки
                 </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                <Tab value="sauce">
                     Соусы
                 </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                <Tab value="main">
                     Начинки
                 </Tab>
             </div>
             <div className={`${style.ingredients} mt-10 ingredients-container`}>
-                <h2 className="mb-6 text text_type_main-medium">{detectCurrentTipeTitle(current)}</h2>
+                <h2 className="mb-6 text text_type_main-medium">Булки</h2>
                 <ul className={`${style.list} pt-6 pb-10 pr-4 pl-4`}>
-                    {startIngredients.map((ingredient) => ingredient.type === current && itemTemplate(ingredient))}
+                    {startIngredients.map((ingredient) => ingredient.type === 'bun' && itemTemplate(ingredient))}
+                </ul>
+                <h2 className="mb-6 text text_type_main-medium">Соусы</h2>
+                <ul className={`${style.list} pt-6 pb-10 pr-4 pl-4`}>
+                    {startIngredients.map((ingredient) => ingredient.type === 'sauce' && itemTemplate(ingredient))}
+                </ul>
+                <h2 className="mb-6 text text_type_main-medium">Начинки</h2>
+                <ul className={`${style.list} pt-6 pb-10 pr-4 pl-4`}>
+                    {startIngredients.map((ingredient) => ingredient.type === 'main' && itemTemplate(ingredient))}
                 </ul>
             </div>
         </div>
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -8,6 +8,21 @@ function checkResult(res) {
   }
 }
 
-export default function getIngredients(url) {
+function sendIngredients (url, ids) {
+  const data = {
+    'ingredients': ids
+  }
+  return fetch(`${url}/orders`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  body: JSON.stringify(data)}).then((res) => checkResult(res)) 
+}
+
+
+function getIngredients(url) {
   return fetch(`${url}/ingredients`).then((res) => checkResult(res));
 }
+
+export default(getIngredients ,sendIngredients)
